d2b892c8423b80ea4c2083c3ac120fcedfc3969a
chiva-emdr <danii@chivaemdr.com>
2026-03-23 20:23:34 -0500

    v1.0.4 Update: v1.0.4

--- _data/site.js
+++ _data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "v1.0.1 Unschool Pages",
+  title: "v1.0.4 Unschool Pages",
   description: "A living notebook for unschooling families.",
   url: "https://unschoolpages.unschooldiscoveries.com",
   brand: "Unschool Discoveries"
